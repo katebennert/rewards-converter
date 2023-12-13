4e846babcac945c9feeedf5c7fdafa0324da7ddf
Kate Bennert <kate.bennert@gmail.com>
2023-12-13 16:03:23 -0500

    Finish conversion unit tests

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,24 +1,30 @@
 public class RewardValue {
-    private double cashValue;
-    private double milesValue;
-    private static final double conversionRate = 0.0035;
+    private final double cashValue;
+    private static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
 
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
-        this.milesValue = cashValue / conversionRate;
+        //this.milesValue = cashValue / conversionRate;
     }
 
     public RewardValue(int milesValue) {
-        this.milesValue = milesValue;
-        this.cashValue = milesValue * conversionRate;
+        this.cashValue = convertToCash(milesValue);
+    }
+
+    private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
+    }
+
+    private static double convertToCash(int milesValue) {
+        return milesValue * MILES_TO_CASH_CONVERSION_RATE;
     }
 
     public double getCashValue() {
         return cashValue;
     }
 
-    public double getMilesValue() {
-        return milesValue;
+    public int getMilesValue() {
+        return convertToMiles(this.cashValue);
     }
 
 }
